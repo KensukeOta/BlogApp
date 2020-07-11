0c080d105bdaad43434d3e0e118054ce0f030e76
KensukeOta <bump_kensuke_miwa@i.softbank.jp>
2020-07-12 01:47:43 +0900

    記事の投稿のバリデーションルールを追加

--- BlogApp/app/Post.php
+++ BlogApp/app/Post.php
@@ -7,4 +7,10 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     //
+    protected $guarded = array('id');
+
+    public static $rules = array(
+        'title' => 'required|min:1|max:50',
+        'body' => 'required|min:1|max:1000',
+    );
 }
