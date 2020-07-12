2124c4cdbdf44432e691551c9ca1a627beb78004
KensukeOta <bump_kensuke_miwa@i.softbank.jp>
2020-07-12 16:31:14 +0900

    記事削除のリンクを追加

--- BlogApp/resources/views/posts/index.blade.php
+++ BlogApp/resources/views/posts/index.blade.php
@@ -13,6 +13,11 @@
             <p class="card-text">{{ $post->body }}</p>
             <a href="{{ action('PostController@show', $post) }}" class="btn btn-success">記事を見る</a>
             <a href="{{ action('PostController@edit', $post) }}" class="btn btn-success">編集</a>
+            <a href="#" class="btn btn-success" data-id="{{ $post->id }}">削除</a>
+            <form method="post" action="{{ url('/posts', $post->id) }}" id="form_{{ $post->id }}">
+                @csrf
+                @method('DELETE')
+            </form>
         </div>
     </div>
     @empty
