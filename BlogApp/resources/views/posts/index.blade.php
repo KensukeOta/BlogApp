47d589f2d0efc6bdc007f9d93aa2f289b24fdbe1
KensukeOta <bump_kensuke_miwa@i.softbank.jp>
2020-07-12 16:39:01 +0900

    記事を削除するロジックと確認ダイアログを表示するように記述

--- BlogApp/resources/views/posts/index.blade.php
+++ BlogApp/resources/views/posts/index.blade.php
@@ -13,7 +13,7 @@
             <p class="card-text">{{ $post->body }}</p>
             <a href="{{ action('PostController@show', $post) }}" class="btn btn-success">記事を見る</a>
             <a href="{{ action('PostController@edit', $post) }}" class="btn btn-success">編集</a>
-            <a href="#" class="btn btn-success" data-id="{{ $post->id }}">削除</a>
+            <a href="#" class="btn btn-success del" data-id="{{ $post->id }}">削除</a>
             <form method="post" action="{{ url('/posts', $post->id) }}" id="form_{{ $post->id }}">
                 @csrf
                 @method('DELETE')
@@ -24,4 +24,5 @@
     <p>現在、記事が投稿されていません。</p>
     @endforelse
 </div>
+<script src="/js/main.js"></script>
 @endsection
