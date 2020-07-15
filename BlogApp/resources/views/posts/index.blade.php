8392399e9cc0c0795dd9fce70fd3f9e61bf8999c
KensukeOta <bump_kensuke_miwa@i.softbank.jp>
2020-07-16 00:26:01 +0900

    ログイン中のユーザー名を上部に表示

--- BlogApp/resources/views/posts/index.blade.php
+++ BlogApp/resources/views/posts/index.blade.php
@@ -4,13 +4,16 @@
 
 @section('content')
 <div class="row">
+    @if (Auth::check())
+    <p class="text-center">ようこそ、{{ $user->name }}さん！</p>
+    @endif
     <a href="/posts/new" class="btn btn-outline-success my-3">投稿する</a>
     @forelse ($posts as $post)
     <div class="card col-md-4">
         <!-- <img src="" class="card-img-top" alt="..."> -->
         <div class="card-body">
             <h5 class="card-title">{{ $post->title }}</h5>
-            <p class="card-text">{{ $post->body }}</p>
+            <p class="card-text">by</p>
             <a href="{{ action('PostController@show', $post) }}" class="btn btn-success">記事を見る</a>
             <a href="{{ action('PostController@edit', $post) }}" class="btn btn-success">編集</a>
             <a href="#" class="btn btn-success del" data-id="{{ $post->id }}">削除</a>
