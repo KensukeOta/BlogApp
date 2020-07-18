763bf9c9259843df03f1d83ff32bd848757580c1
KensukeOta <bump_kensuke_miwa@i.softbank.jp>
2020-07-19 05:03:14 +0900

    全体の背景色とコンテンツの背景色を指定

--- BlogApp/resources/views/layouts/default.blade.php
+++ BlogApp/resources/views/layouts/default.blade.php
@@ -7,7 +7,7 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/css/bootstrap.min.css" integrity="sha384-r4NyP46KrjDleawBgD5tp8Y7UzmLA05oM1iAEQ17CSuDqnUK2+k9luXQOfXJCJ4I" crossorigin="anonymous">
     <link rel="stylesheet" href="/css/styles.css">
 </head>
-<body>
+<body style="background: #f6f6f4;">
     <header class="navbar bg-success">
         <nav class="container">
             <a href="/" class="text-light text-decoration-none">BlogApp</a>
@@ -19,7 +19,7 @@
             @endif
         </nav>
     </header>
-    <div class="container">
+    <div class="container my-5" style="padding: 32px; background: #fff;">
         @yield('content')
     </div>
     
