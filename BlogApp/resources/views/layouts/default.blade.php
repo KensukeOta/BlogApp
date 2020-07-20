e473ea7be35c0027835da28a9d5af9be91bc5e98
KensukeOta <bump_kensuke_miwa@i.softbank.jp>
2020-07-20 20:26:24 +0900

    fontawesomeのCDNを読み込みウェブフォントを記述

--- BlogApp/resources/views/layouts/default.blade.php
+++ BlogApp/resources/views/layouts/default.blade.php
@@ -5,6 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title')</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/css/bootstrap.min.css" integrity="sha384-r4NyP46KrjDleawBgD5tp8Y7UzmLA05oM1iAEQ17CSuDqnUK2+k9luXQOfXJCJ4I" crossorigin="anonymous">
+    <script src="https://kit.fontawesome.com/24fb70e414.js" crossorigin="anonymous"></script>
+  </head>
     <link rel="stylesheet" href="/css/styles.css">
 </head>
 <body style="background: #f6f6f4;">
