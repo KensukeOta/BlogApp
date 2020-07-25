f14c7b23a1abfcd13901513838d78799d27c3d2b
KensukeOta <bump_kensuke_miwa@i.softbank.jp>
2020-07-25 19:05:47 +0900

    postsテーブルとlikesテーブルのリレーションを記述

--- BlogApp/app/Post.php
+++ BlogApp/app/Post.php
@@ -7,12 +7,14 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     //
-    protected $guarded = array('id');
+    protected $guarded = ['id'];
 
-    public static $rules = array(
+
+
+    public static $rules = [
         'title' => 'required|min:1|max:50',
         'body' => 'required|min:1|max:1000',
-    );
+    ];
 
     public function user()
     {
@@ -22,5 +24,10 @@ class Post extends Model
     public function comments()
     {
         return $this->hasMany('App\Comment');
+    }
+
+    public function likes()
+    {
+        return $this->hasMany('App\Like');
     }
 }
