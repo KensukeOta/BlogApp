d3dcb2b82ad10636c62cbba7850365fa1d521f99
KensukeOta <bump_kensuke_miwa@i.softbank.jp>
2020-07-28 19:41:10 +0900

    componentsにArticleLikeコンポーネントを登録

--- BlogApp/resources/js/app.js
+++ BlogApp/resources/js/app.js
@@ -4,10 +4,10 @@
  * building robust, powerful web applications using Vue and Laravel.
  */
 
-require('./bootstrap');
-
-window.Vue = require('vue');
-
+import "./bootstrap";
+import Vue from "vue";
+import ArticleLike from "./components/ArticleLike";
+ 
 /**
  * The following block of code may be used to automatically register your
  * Vue components. It will recursively scan this directory for the Vue
@@ -19,8 +19,6 @@ window.Vue = require('vue');
 // const files = require.context('./', true, /\.vue$/i)
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
 
-Vue.component('example-component', require('./components/ExampleComponent.vue').default);
-
 /**
  * Next, we will create a fresh Vue application instance and attach it to
  * the page. Then, you may begin adding components to this application
@@ -28,5 +26,8 @@ Vue.component('example-component', require('./components/ExampleComponent.vue').
  */
 
 const app = new Vue({
-    el: '#app',
+    el: "#app",
+    components: {
+        ArticleLike
+    }
 });
