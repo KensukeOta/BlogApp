b32be0325609bcfdac44bbff58e514966163eaaa
KensukeOta <bump_kensuke_miwa@i.softbank.jp>
2020-07-29 12:03:43 +0900

    ユーザーをフォロー中かどうか判定するメソッドを記述

--- BlogApp/app/User.php
+++ BlogApp/app/User.php
@@ -52,4 +52,12 @@ class User extends Authenticatable
     {
         return $this->belongsToMany('App\User', 'follows', 'followee_id', 'follower_id')->withTimestamps();
     }
+
+    //  ユーザーをフォロー中かどうか判定するメソッド
+    public function isFollowedBy(?User $user): bool
+    {
+        return $user
+            ? (bool)$this->followers->where('id', $user->id)->count()
+            : false;
+    }
 }
