29a47155ca0d46b4e5ff1d55b13a870eccfcd9ad
KensukeOta <bump_kensuke_miwa@i.softbank.jp>
2020-07-29 21:24:03 +0900

    ユーザーページのお気に入り記事リンクが押された際のルーティングを記述

--- BlogApp/routes/web.php
+++ BlogApp/routes/web.php
@@ -37,9 +37,9 @@ Route::prefix('posts')->name('posts.')->group(function () {
 
 
 Route::prefix('users')->name('users.')->group(function () {
-    Route::get('/{user:name}', 'UserController@home');
-    Route::get('/{user:name}/posts', 'UserController@index');
     Route::get('/{user:name}', 'UserController@home')->name('home');
+    Route::get('/{user:name}/posts', 'UserController@index');
+    Route::get('/{user:name}/likes', 'UserController@likes')->name('likes');
     Route::middleware('auth')->group(function () {
         Route::put('/{user:name}/follow', 'UserController@follow')->name('follow');
         Route::delete('/{user:name}/follow', 'UserController@unfollow')->name('unfollow');
