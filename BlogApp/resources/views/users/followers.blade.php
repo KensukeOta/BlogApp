5b6736681fa28547f7ae29fbcaf5406013a49b4e
KensukeOta <bump_kensuke_miwa@i.softbank.jp>
2020-07-31 20:12:59 +0900

    ユーザーページの各ページにcontainerクラスを付与し、間隔を共通化

--- BlogApp/resources/views/users/followers.blade.php
+++ BlogApp/resources/views/users/followers.blade.php
@@ -3,27 +3,29 @@
 @section('title', $selectUser->name . 'さんのフォロワー' . ' - ' . 'BlogApp')
 
 @section('content')
-<p class="text-center">{{ $selectUser->name }}さんのフォロワー</p>
-@forelse ($followers as $person)
-<div class="card col">
-    <!-- <img src="" class="card-img-top" alt="..."> -->
-    <div class="card-body">
-        <div class="d-flex justify-content-between">
-            <h5 class="card-title">{{ $person->name }}</h5>
-            @if (Auth::check())
-            @unless (Auth::user()->name === $person->name)
-            <follow-button
-            :initial-is-followed-by='@json($person->isFollowedBy(Auth::user()))'
-            :authorized='@json(Auth::check())'
-            endpoint="{{ route('users.follow', $person->name) }}"
-            ></follow-button>
-            @endunless
-            @endif
+<div class="container">
+    <p class="text-center">{{ $selectUser->name }}さんのフォロワー</p>
+    @forelse ($followers as $person)
+    <div class="card col">
+        <!-- <img src="" class="card-img-top" alt="..."> -->
+        <div class="card-body">
+            <div class="d-flex justify-content-between">
+                <h5 class="card-title">{{ $person->name }}</h5>
+                @if (Auth::check())
+                @unless (Auth::user()->name === $person->name)
+                <follow-button
+                :initial-is-followed-by='@json($person->isFollowedBy(Auth::user()))'
+                :authorized='@json(Auth::check())'
+                endpoint="{{ route('users.follow', $person->name) }}"
+                ></follow-button>
+                @endunless
+                @endif
+            </div>
         </div>
     </div>
+    @empty
+    <p>フォロワーがいません</p>
+    @endforelse
 </div>
-@empty
-<p>フォロワーがいません</p>
-@endforelse
 <script src="/js/main.js"></script>
 @endsection
--- BlogApp/resources/views/users/index.blade.php
+++ BlogApp/resources/views/users/index.blade.php
@@ -3,41 +3,43 @@
 @section('title', $selectUser->name . 'さんの記事一覧' . ' - ' .'BlogApp')
 
 @section('content')
-<div class="row">
-    <p class="text-center">{{ $selectUser->name }}さんの投稿</p>
-    @forelse ($selectUser->posts as $post)
-    <div class="card col-md-4">
-        <!-- <img src="" class="card-img-top" alt="..."> -->
-        <div class="card-body">
-            <div class="d-flex justify-content-between">
-                <h5 class="card-title">{{ $post->title }}</h5>
-                <article-like
-                :initial-is-liked-by='@json($post->isLikedBy(Auth::user()))'
-                :initial-count-likes='@json($post->count_likes)' 
-                :authorized='@json(Auth::check())'
-                endpoint="{{ route('posts.like', ['post' => $post]) }}"    
-                >
-                </article-like>
+<div class="container">
+    <div class="row">
+        <p class="text-center">{{ $selectUser->name }}さんの投稿</p>
+        @forelse ($selectUser->posts as $post)
+        <div class="card col-md-4">
+            <!-- <img src="" class="card-img-top" alt="..."> -->
+            <div class="card-body">
+                <div class="d-flex justify-content-between">
+                    <h5 class="card-title">{{ $post->title }}</h5>
+                    <article-like
+                    :initial-is-liked-by='@json($post->isLikedBy(Auth::user()))'
+                    :initial-count-likes='@json($post->count_likes)' 
+                    :authorized='@json(Auth::check())'
+                    endpoint="{{ route('posts.like', ['post' => $post]) }}"    
+                    >
+                    </article-like>
+                </div>
+                <p class="card-text">by {{ $post->user->name }}</p>
+                <a href="{{ action('PostController@show', $post) }}" class="btn btn-success">記事を見る</a>
+                @if (Auth::check())
+                    @if (Auth::user()->id === $post->user_id)
+                    <a href="{{ action('PostController@edit', $post) }}" class="btn btn-success">編集</a>
+                    @endif
+                    @if (Auth::user()->id === $post->user_id)
+                    <a href="#" class="btn btn-success del" data-id="{{ $post->id }}">削除</a>
+                    @endif
+                    <form method="post" action="{{ url('/posts', $post->id) }}" id="form_{{ $post->id }}">
+                        @csrf
+                        @method('DELETE')
+                    </form>
+                @endif
             </div>
-            <p class="card-text">by {{ $post->user->name }}</p>
-            <a href="{{ action('PostController@show', $post) }}" class="btn btn-success">記事を見る</a>
-            @if (Auth::check())
-                @if (Auth::user()->id === $post->user_id)
-                <a href="{{ action('PostController@edit', $post) }}" class="btn btn-success">編集</a>
-                @endif
-                @if (Auth::user()->id === $post->user_id)
-                <a href="#" class="btn btn-success del" data-id="{{ $post->id }}">削除</a>
-                @endif
-                <form method="post" action="{{ url('/posts', $post->id) }}" id="form_{{ $post->id }}">
-                    @csrf
-                    @method('DELETE')
-                </form>
-            @endif
         </div>
+        @empty
+        <p>現在、記事が投稿されていません。</p>
+        @endforelse
     </div>
-    @empty
-    <p>現在、記事が投稿されていません。</p>
-    @endforelse
 </div>
 <script src="/js/main.js"></script>
 @endsection
