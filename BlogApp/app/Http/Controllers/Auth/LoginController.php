b1ad4be9385f5aa061b6543e4fdf03399cf57f44
KensukeOta <bump_kensuke_miwa@i.softbank.jp>
2020-08-02 09:27:56 +0900

    Googleログインボタンを設置

--- BlogApp/app/Http/Controllers/Auth/LoginController.php
+++ BlogApp/app/Http/Controllers/Auth/LoginController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Laravel\Socialite\Facades\Socialite;
 
 class LoginController extends Controller
 {
@@ -20,7 +21,6 @@ class LoginController extends Controller
     */
 
     use AuthenticatesUsers;
-    use Laravel\Socialite\Facades\Socialite;
 
     /**
      * Where to redirect users after login.
