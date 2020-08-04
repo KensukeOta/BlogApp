485310325d7eb8077f0f0209d98bedfc30a5e7da
KensukeOta <bump_kensuke_miwa@i.softbank.jp>
2020-08-04 12:12:30 +0900

    パスワード再設定画面の作成

--- BlogApp/resources/views/auth/passwords/reset.blade.php
+++ BlogApp/resources/views/auth/passwords/reset.blade.php
@@ -4,60 +4,42 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Reset Password') }}</div>
-
-                <div class="card-body">
-                    <form method="POST" action="{{ route('password.update') }}">
-                        @csrf
-
-                        <input type="hidden" name="token" value="{{ $token }}">
-
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ $email ?? old('email') }}" required autocomplete="email" autofocus>
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
+            <div class="login-content">
+                <form method="POST" action="{{ route('password.update') }}">
+                    @csrf
+                    <h2 class="title">新しいパスワードを設定</h2>
+                    <input type="hidden" name="email" value="{{ $email }}">
+                    <input type="hidden" name="token" value="{{ $token }}">
+                    <div class="input-div one">
+                        <div class="i">
+                            <i class="fas fa-lock"></i>
                         </div>
-
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
+                        <div class="div">
+                            <h5>新しいパスワード</h5>
+                            <input type="password" class="input @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
+        
+                            @error('password')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                            @enderror
                         </div>
+                    </div>
 
-                        <div class="form-group row">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
-                            </div>
+                    <div class="input-div pass">
+                        <div class="i">
+                            <i class="fas fa-lock"></i>
                         </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Reset Password') }}
-                                </button>
-                            </div>
+                        <div class="div">
+                            <h5>新しいパスワード(再入力)</h5>
+                            <input type="password" class="input" name="password_confirmation" required autocomplete="new-password">
                         </div>
-                    </form>
-                </div>
+                    </div>
+
+                    <div class="row">
+                        <input type="submit" class="submit" value="送信">
+                    </div>
+                </form>
             </div>
         </div>
     </div>
--- infrastructure/docker/php/bash/.bash_history
+++ infrastructure/docker/php/bash/.bash_history
@@ -1,3 +1,6 @@
 php artisan config:cache
 php artisan config:cache
 exit
+php artisan make:mail BareMail
+php artisan make:notification PasswordResetNotification
+exit
