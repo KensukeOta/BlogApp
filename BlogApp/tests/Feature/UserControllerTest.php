a91efc204164dda48c2a1bdfc1076fe4c82aa5a4
KensukeOta <bump_kensuke_miwa@i.softbank.jp>
2020-08-15 11:49:15 +0900

    ダミーのユーザーデータを作成し、テスト実行後にDBをロールバックし、テスト実行中にDBに作ったデータを残らなくするためにDatabaseTransactionsを記述

--- BlogApp/tests/Feature/UserControllerTest.php
+++ BlogApp/tests/Feature/UserControllerTest.php
@@ -2,6 +2,8 @@
 
 namespace Tests\Feature;
 
+use App\User;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
@@ -13,8 +15,12 @@ class UserControllerTest extends TestCase
      *
      * @return void
      */
+    use DatabaseTransactions;
+    
     public function testExample()
     {
+        $user = factory(User::class)->create();
+
         $response = $this->get('/');
 
         $response->assertStatus(200);
--- infrastructure/docker/php/bash/.bash_history
+++ infrastructure/docker/php/bash/.bash_history
@@ -1,2 +1,3 @@
-php artisan make:test UserControllerTest
+php artisan test
+php artisan test
 exit
